4f016832cd471403db6ed9bae84cdbb18b0c9665
Afrah Aden <afrahaden05@gmail.com>
2023-04-10 22:16:22 +0300

    initial commit

---
+++ hello.exs
@@ -0,0 +1,11 @@
+# Question one
+
+defmodule Blogger do
+  use HTTPoison.Base
+
+  @url "https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91"
+
+  def fetch_blog do
+    HTTPoison.get(@url)
+  end
+end
